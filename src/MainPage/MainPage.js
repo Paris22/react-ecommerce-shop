f1710652b23a8fa0754e7cb6156ae542e310a3cf
Vladislav Latanov <lvn1722@yandex.ru>
2022-06-29 17:21:48 +0300

    Main page header initial

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Card from "../Cards/Card";
+import Image from "mui-image";
+import {Input} from "@mui/material";
+import {MarginHeader} from "../Configs/Global.styled";
 
 
 const MainPage = () => {
     return (
-        <div>
-
+        <div className="Content">
+            <MarginHeader>
+                <div className="ContentHeader">
+                    <h1>Все товары</h1>
+                    <div className="Search-block">
+                        <Image src="" alt="SearchButton"/>
+                        <Input placeholder="Поиск..."/>
+                    </div>
+                </div>
+                <Card/>
+            </MarginHeader>
         </div>
     );
 };
